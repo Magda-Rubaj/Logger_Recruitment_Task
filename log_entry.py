fedf74c83d7a89d8ba8da54f73067d89767e4f32
Magda-Rubaj <magiru@o2.pl>
2021-06-20 14:56:47 +0200

    Set up profil logger and log entry classes skeleton

---
+++ log_entry.py
@@ -0,0 +1,5 @@
+
+class LogEntry(object):
+
+    def __init__(self, date, level, msg) -> None:
+        pass
---
+++ profil_logger.py
@@ -0,0 +1,23 @@
+
+class ProfilLogger(object):
+    
+    def __init__(self, handlers) -> None:
+        pass
+
+    def info(msg: str) -> None:
+        pass
+    
+    def warning(msg: str) -> None:
+        pass
+
+    def critical(msg: str) -> None:
+        pass
+        
+    def error(msg: str) -> None:
+        pass
+
+    def debug(msg: str) -> None:
+        pass
+
+    def set_log_level(msg: str) -> None:
+        pass
