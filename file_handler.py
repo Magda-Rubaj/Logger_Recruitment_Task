cbaa9ba86a881ece46da9e658359824d1681773f
Magda-Rubaj <magiru@o2.pl>
2021-06-27 17:23:37 +0200

    added file handler

---
+++ file_handler.py
@@ -0,0 +1,22 @@
+import pickle
+from typing import List
+from handler import Handler
+from log_entry import LogEntry
+
+
+class FileHandler(Handler):
+    def __init__(self, file: str) -> None:
+        super().__init__(file)
+
+    def save(self, log) -> None:
+        with open(self.file, 'ab') as f:
+            pickle.dump(log, f)
+    def retrieve(self) -> List[LogEntry]:
+        logs = []
+        with open(self.file, 'rb') as f:
+            while 1:
+                try:
+                    logs.append(pickle.load(f))
+                except EOFError:
+                    break
+        return logs
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from csv_handler import CSVHandler
 from json_handler import JsonHandler
 from sql_handler import SQLHandler
+from file_handler import FileHandler
 from logger_reader import ProfilLoggerReader
 from profil_logger import ProfilLogger
 from datetime import datetime
@@ -8,10 +9,11 @@ from datetime import datetime
 json_handler = JsonHandler("logs.json")
 csv_handler = CSVHandler("logs.csv")
 sql_handler = SQLHandler("logs.sqlite")
+file_handler = FileHandler("logs.txt")
 
-logger = ProfilLogger(handlers=[json_handler, csv_handler, sql_handler])
+logger = ProfilLogger(handlers=[json_handler, csv_handler, sql_handler, file_handler])
 logger.info("Some message")
-log_reader = ProfilLoggerReader(handler=sql_handler)
+log_reader = ProfilLoggerReader(handler=file_handler)
 start = datetime(2021, 6, 26, 19, 56, 1)
 end = datetime(2021, 6, 26, 20, 8, 10)
 print(log_reader.groupby_level())
